a600547f1867b0f9537512cde423116494f9ce7d
Abhi <mallepalliabhishek@gmail.com>
2020-11-14 17:35:51 -0800

    added amqplib and hello world code

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,45 @@
 import express from "express";
 const app = express();
 const port = 8080; // default port to listen
-require('dotenv').config()
+import {config} from "dotenv";
+import { resolve } from "path";
+config({path:resolve(__dirname,".env")})
+import {connect, Connection, ConfirmChannel, Channel, credentials}  from  'amqplib/callback_api';
+
+
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
     res.send( "Hello world!" );
 } );
 
 // start the Express server
-app.listen( port, () => {
+const server = app.listen( port, () => {
+    connect('amqp://localhost', (error0, connection)=>{
+        if(error0){
+            throw error0
+        }else{
+            // tslint:disable-next-line:no-console
+            console.log("Horray. We connected to RabbitMQ")
+        }
+        connection.createChannel((error1,channel)=>{
+            if (error1) {
+              throw error1;
+            }else{
+                // tslint:disable-next-line:no-console
+                console.log("Horray. We created a channel")
+            }
+            const queue = 'hello';
+            const msg = 'Hello world';
+
+            channel.assertQueue(queue, {
+              durable: false
+            });
+
+            channel.sendToQueue(queue, Buffer.from(msg));
+            // tslint:disable-next-line:no-console
+            console.log(" [x] Sent %s", msg);
+          });
+    });
     // tslint:disable-next-line:no-console
     console.log( `server started at http://localhost:${ port }` );
 } );
