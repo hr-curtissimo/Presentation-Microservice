13c3cef02df67630b66f0194c335d5556ae99027
Abhi <mallepalliabhishek@gmail.com>
2020-11-15 20:23:27 -0800

    added some comments

--- src/dbinit.ts
+++ src/dbinit.ts
@@ -6,7 +6,7 @@ dotenv.config();
 import * as bluebird from "bluebird"
 import {createPresentation,state, getAllPresentations, getPresentation, modifyPresentationState} from "./dbcontroller"
 async function init(){
-    //connects to database
+    // connects to database
     const connection = await createConnection({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
@@ -23,25 +23,25 @@ async function init(){
         console.log('connected as id ' + connection.threadId);
     });
 
-    //destroys the old database if it exists
+    // destroys the old database if it exists
     await connection.promise().query('DROP DATABASE IF EXISTS presentationproposals')
     .catch(console.log)
     .then(()=>{
         console.log("presentationproposals database dropped")
     });
-    //creates a new database called presentationproposals
+    // creates a new database called presentationproposals
     await connection.promise().query('CREATE DATABASE presentationproposals')
     .catch(console.log)
     .then(()=>{
         console.log("presentationproposals database created")
     });
-    //switches to database we just created
+    // switches to database we just created
     await connection.promise().query("USE presentationproposals")
     .catch(console.log)
     .then(()=>{
         console.log("presentationproposals database selected");
     })
-    //creates the table to store the presentations, called presentations
+    // creates the table to store the presentations, called presentations
     await connection.promise().query("CREATE TABLE presentations (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, event varchar(125), speakername varchar(125), email varchar(125), company varchar(125), title varchar(125), synopsis LONGTEXT, state enum('submitted', 'approved', 'not this year'))")
     .catch(console.log)
     .then(()=>{
@@ -49,7 +49,7 @@ async function init(){
     })
 
 
-    //Creates a sample presentation at at sample event
+    // Creates a sample presentation at at sample event
     await createPresentation(connection, {
         event:"sampleevent",
         email:"asdf@asdf.com",
@@ -59,23 +59,23 @@ async function init(){
         synopsis:"this isa  syopsis of asdfasdfasdf asdftitle presentation",
         state:state.submitted
     })
-    
-    //Retrieves all presentations
+
+    // Retrieves all presentations
     const presentations = await getAllPresentations(connection, "sampleevent");
     console.log("retreiving the presentations for sampleevent",presentations[0]);
 
-    //Retrieves just the sample presentation we just created
+    // Retrieves just the sample presentation we just created
     let presentation = await getPresentation(connection, "sampleevent", "asdftitle")
     console.log("retreiving the presentation asdftitle taking place at sampleevent", presentation[0]);
 
-    //Approves just the sample presentation we just created
+    // Approves just the sample presentation we just created
     await modifyPresentationState(connection,"sampleevent", "asdftitle",state.approved);
 
-    //Retrieves the sample presentation again
+    // Retrieves the sample presentation again
     presentation = await getPresentation(connection, "sampleevent", "asdftitle")
     console.log("presentation asdftitle at sampleevent should be approved", presentation[0]);
 
-    //Kills the connection
+    // Kills the connection
     await connection.end();
 }
 init();
